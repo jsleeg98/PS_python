1d92a702857fa58a202dcbcba110545808d2321b
Laptop <jsleeg7279@naver.com>
2024-02-20 16:16:32 +0900

    [BOJ] 14938 서강그라운드 - 골드4

---
+++ backjoon/14938 서강그라운드.py
@@ -0,0 +1,51 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+INF = int(1e9)
+
+n, m, r = map(int, sys.stdin.readline().split())
+items = list(map(int, sys.stdin.readline().split()))
+items.insert(0, 0)
+
+graph = [[] for i in range(n + 1)]
+
+for i in range(r):
+    a, b, l = map(int, sys.stdin.readline().split())
+    graph[a].append((b, l))
+    graph[b].append((a, l))
+
+dist_map = [[INF for j in range(n + 1)]for i in range(n + 1)]
+
+for s in range(1, len(graph)):
+    for g in graph[s]:
+        e, l = g
+        dist_map[s][e] = l
+
+for i in range(1, len(graph)):
+    dist_map[i][i] = 0
+
+# for i in range(1, len(graph)):
+#     for j in range(1, len(graph)):
+#         print(dist_map[i][j], end=' ')
+#     print()
+
+for k in range(1, len(graph)):
+    for i in range(1, len(graph)):
+        for j in range(1, len(graph)):
+            if dist_map[i][j] > dist_map[i][k] + dist_map[k][j]:
+                dist_map[i][j] = dist_map[i][k] + dist_map[k][j]
+
+# for i in range(1, len(graph)):
+#     for j in range(1, len(graph)):
+#         print(dist_map[i][j], end=' ')
+#     print()
+
+result = [0 for i in range(n + 1)]
+for i in range(1, len(graph)):
+    for j in range(1, len(graph)):
+        if dist_map[i][j] <= m:
+            result[i] += items[j]
+
+# print(result)
+print(max(result))
